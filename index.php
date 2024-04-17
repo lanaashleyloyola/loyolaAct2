394cf9d99ed85c37093b506882fb9c9638857f42
lanaashleyloyola <167303431+lanaashleyloyola@users.noreply.github.com>
2024-04-17 02:57:03 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Form with CSS</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Login Form</h2>
+        <form action="/login" method="post">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+            <center><a href="dasboard.php"><input type="Button" value="LOG IN"></center>
+        </form>
+    </div>
+</body>
+</html>
